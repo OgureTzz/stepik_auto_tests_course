01a05be3c34c477a5d789334caa0ffaca63cf953
OgureTzz <104894696+OgureTzz@users.noreply.github.com>
2022-05-26 14:06:05 +0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
-
-def test_guest_should_see_login_link_pass(browser):
-    browser.get(link)
-    assert browser.find_element(By.XPATH, "//button[@value]").text == 'Add to basket'
-
